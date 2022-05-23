a0b00413a2bc8856089bd2d49a065d391c6a0d62
CrAzI-code <emmaobiora96@gmail.com>
2022-05-23 12:58:42 -0700

    finished chat system

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from optparse import Values
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect
+from chat.models import Room, Message
 
 # Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+def room(request, room):
+    username = request.GET.get('username')
+    room_detail = Room.objects.get(name=room)    
+
+    return render(request, "room.html",
+    {
+    'username': username,
+    'room': room,
+    'room_detail':room_detail
+    })
+
+def checkview(request):
+        room = request.POST['room_name']
+        username = request.POST['username']
+
+        if Room.objects.filter(name=room).exists():
+            return redirect('/'+room+'/?username='+username)
+        else:
+            new_room = Room.objects.create(name=room)
+            new_room.save()
+            return redirect('/'+room+'/?username='+username)
+
+
+def send(request):
+    message = request.POST['message']
+    username = request.POST['username']
+    room_id = request.POST['room_id']
+
+    new_message = Message.objects.create(value = message, user = username, room = room_id)
+    new_message.save()
+    return HttpResponse('Message Sent Successfully')
+
+
+def getMessage(request, room):
+    room_details = Room.objects.get(name=room)
+
+    messages = Message.objects.filter(room=room_details.id)
+    return JsonResponse({'messages': list(messages.values())})
